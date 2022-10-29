66288e958acbead9815aaaf2b5dac066e1638c6b
Duy Hưng <ldhung311@gmail.com>
2022-10-29 11:11:45 +0700

    map added to speech report

--- app/src/main/java/com/hcmut/admin/utrafficsystem/ui/report/speech/SpeechReportFragment.java
+++ app/src/main/java/com/hcmut/admin/utrafficsystem/ui/report/speech/SpeechReportFragment.java
@@ -1,30 +1,44 @@
 package com.hcmut.admin.utrafficsystem.ui.report.speech;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.hcmut.admin.utrafficsystem.R;
 import com.hcmut.admin.utrafficsystem.ui.map.MapActivity;
+import com.google.android.gms.maps.MapView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SpeechReportFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SpeechReportFragment extends Fragment implements MapActivity.OnBackPressCallback {
+public class SpeechReportFragment extends Fragment implements MapActivity.OnBackPressCallback, OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private MapView mapView;
+    private GoogleMap gMap;
+    private ImageButton record;
+    private Button submit;
+    private MediaRecorder myAudioRecorder;
+    private SupportMapFragment mapFragment;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -62,13 +76,19 @@ public class SpeechReportFragment extends Fragment implements MapActivity.OnBack
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        addControls(view);
+        addControls(view, savedInstanceState);
         addEvents();
     }
 
-    private void addControls(View view) {
+    private void addControls(View view, Bundle savedInstanceState) {
         try {
             ((MapActivity) view.getContext()).hideBottomNav();
+            record = view.findViewById(R.id.speechRecordButton);
+            submit = view.findViewById(R.id.btnSubmitSpeechReport);
+            mapView = view.findViewById(R.id.mapView);
+            mapView.onCreate(savedInstanceState);
+            mapView.onResume();
+            mapView.getMapAsync(this);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -86,4 +106,14 @@ public class SpeechReportFragment extends Fragment implements MapActivity.OnBack
     public void onBackPress() {
         NavHostFragment.findNavController(SpeechReportFragment.this).popBackStack();
     }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (googleMap != null) {
+            googleMap.setMyLocationEnabled(true);
+            gMap = googleMap;
+        }
+    }
+
 }
